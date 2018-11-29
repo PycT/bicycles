75ee6e55ca5978bbfa3e0bbaadce6321d241dddb
Rustem Zakiev <rustemzakiev@MacBook-Pro.local>
2018-11-29 12:47:57 +0300

    calc

--- calc/calc.c
+++ calc/calc.c
@@ -7,23 +7,27 @@ int main(int argc, char* argv[]){
 	if (argc != 4){
 		printf("\n calc needs 3 arguments - operator1 operation operator2. E.g. \"calc 2 + 2\" \n");
 		printf("	asterisk should be escaped: \"calc 2 \\* 2\" \n\n");
-		//return 4;
+		return 4;
 	}
+
+	float res;
 
 	switch(argv[2][0]){
 		case '*':
-			printf("\n%.2f\n\n", strtof(argv[1], NULL) * strtof(argv[3], NULL));
+			res = strtof(argv[1], NULL) * strtof(argv[3], NULL);
 			break;
 		case '+':
-			printf("\n%.2f\n\n", strtof(argv[1], NULL) + strtof(argv[3], NULL));
+			res = strtof(argv[1], NULL) + strtof(argv[3], NULL);
 			break;
 		case '-':
-			printf("\n%.2f\n\n", strtof(argv[1], NULL) - strtof(argv[3], NULL));
+			res = strtof(argv[1], NULL) - strtof(argv[3], NULL);
 			break;
 		case '/':
-			printf("\n%.2f\n\n", strtof(argv[1], NULL) / strtof(argv[3], NULL));
+			res = strtof(argv[1], NULL) / strtof(argv[3], NULL);
 			break;
 	}
+
+	printf("\n%.2f\n\n", res);
 
 	return 0;
 }
