a408cc5f734d7b7ba397e271b8bbf909ca51b57f
Rustem Zakiev <rustemzakiev@MacBook-Pro.local>
2018-12-07 18:41:58 +0300

    made proper multiplying

--- calc/calc.c
+++ calc/calc.c
@@ -4,13 +4,19 @@
 
 int main(int argc, char* argv[]){
 
-	if (argc != 4){
+	if (argc < 4){
 		printf("\n calc needs 3 arguments - operator1 operation operator2. E.g. \"calc 2 + 2\" \n");
-		printf("	asterisk should be escaped: \"calc 2 \\* 2\" \n\n");
+		//printf("	asterisk should be escaped: \"calc 2 \\* 2\" \n\n");
 		return 4;
 	}
 
 	float res;
+
+
+	if (argc > 4){
+		res = strtof(argv[1], NULL) * strtof(argv[argc - 1], NULL);
+	}
+	else
 
 	switch(argv[2][0]){
 		case '*':
